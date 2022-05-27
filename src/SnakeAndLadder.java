8522976faca84a762294687b329e6e5623d2a75b
Inamul <inamulhoque097@gmail.com>
2022-05-27 21:06:00 +0530

    Added: Options of Snake, Ladder and No Play

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
         System.out.println("Starting position of player : "+playerPosition);
         int dice = (int)(Math.random()*10)%6+1;
         System.out.println("Dice number: "+dice);
+
+        int No_Play = 1;
+        int Ladder = 2;
+        int Snake = 3;
+        int option = (int)(Math.random()*10)%3+1;
+        System.out.println("\nOption 1 : No Play");
+        System.out.println("Option 2 : Ladder");
+        System.out.println("Option 3 : Snake\n");
+        System.out.println("The player got: "+option);
+        switch (option){
+            case 1:
+                System.out.println("No Play");
+                break;
+            case 2:
+                if (playerPosition<=100) {
+                    playerPosition = playerPosition + dice;
+                }
+                break;
+            case 3:
+                playerPosition=playerPosition-dice;
+                if (playerPosition-dice >=0) {
+                    playerPosition=playerPosition-dice;
+                }
+                break;
+        }
+        if (0 <= playerPosition && playerPosition < 100){
+            System.out.println("Player's position is:" + playerPosition);
+        } else if (playerPosition==100) {
+            System.out.println("You have won!!!");
+        } else if (playerPosition<0){
+            System.out.println();
+        }
+
     }
 }
