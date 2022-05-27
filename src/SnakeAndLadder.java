98cc8ec03fc1c4c6b32dc3e46ee2e57f190baf95
Inamul <inamulhoque097@gmail.com>
2022-05-27 20:17:28 +0530

    Added: The player rolls the dice to get random number between 1 and 6

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,5 +2,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
         System.out.println("Starting position of player : "+playerPosition);
+        int dice = (int)(Math.random()*10)%6+1;
+        System.out.println("Dice number: "+dice);
     }
 }
