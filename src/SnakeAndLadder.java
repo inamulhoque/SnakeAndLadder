9f15665174173b5ad2e74e9ae07959681d03152d
Inamul <inamulhoque097@gmail.com>
2022-06-01 00:05:39 +0530

    Added: Display player position everytime dice rolled and how many times rolled to win

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -6,6 +6,8 @@ public class SnakeAndLadder {
         System.out.println("\nOption 1 : No Play");
         System.out.println("Option 2 : Ladder");
         System.out.println("Option 3 : Snake\n");
+        int count =1;
+
 
         for (playerPosition=1;playerPosition<=100;playerPosition++) {
             int dice = (int)(Math.random()*10)%6+1;
@@ -14,27 +16,32 @@ public class SnakeAndLadder {
             int Ladder = 2;
             int Snake = 3;
             int option = (int) (Math.random() * 10) % 3 + 1;
+            count++;
             System.out.println("The player got: " + option);
             switch (option) {
                 case 1:
                     System.out.println("No Play");
                     playerPosition = playerPosition + 0;
+                    System.out.println("Player Position after dice roll:"+playerPosition);
                     break;
                 case 2:
                     if (playerPosition <= 100) {
                         playerPosition = playerPosition + dice;
                     }
+                    System.out.println("Player Position after dice roll:"+playerPosition);
                     break;
                 case 3:
                     playerPosition = playerPosition - dice;
                     if (playerPosition < 0) {
                         playerPosition = 0;
                     }
+                    System.out.println("Player Position after dice roll:"+playerPosition);
                     break;
             }
             System.out.println("Player position: "+playerPosition);
             if (playerPosition==100){
                 System.out.println("Congratulation!!! you have won.");
+                System.out.println(count+" number of times dice rolled.");
             } else if (playerPosition>100){
                 playerPosition=playerPosition-dice;
             }
